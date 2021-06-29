48af7623e04f023ed7180d92bf82c96194eaa183
twtwtw-gj <twtwtw-gj@users.noreply.github.com>
2021-06-29 09:28:44 +0000

    deploy: 00c970de6623b10a4b727c65d12ebdc0603faf9f

---
+++ component---src-pages-index-js-03eed3466549dd770314.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{7704:function(e,t,l){"use strict";l.r(t);var n=l(7294),r=l(5444),a=l(5127),u=l(3751);t.default=function(){return n.createElement(a.Z,null,n.createElement(u.Z,{title:"Home"}),n.createElement("h1",null,"Hi people"),n.createElement("p",null,n.createElement(r.Link,{to:"profile/"},"Go to profile")," ",n.createElement("br",null)))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-03eed3466549dd770314.js.map
