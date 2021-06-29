4c39fc1456cab675920f6f705966e8e19057f419
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2021-06-29 09:15:24 +0000

    create my site

---
+++ src/pages/profile.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const ProfilePage = () => (
+  <Layout>
+    <h1>Profile</h1>
+    <p>I'm twtwtw-gj.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default ProfilePage
