e107eb11e55f51cf5933653f53e7a79a4f04844c
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2021-07-08 10:25:06 +0000

    make title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `twtwtw-gj portofolio`,
+    description: `twtwtw-gj portofolio`,
+    author: `twtwtw-gj`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
