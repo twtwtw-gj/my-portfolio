34f60a15328f80ccdc7735752c057d38a24d5cf6
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2021-07-25 13:41:48 +0900

    Update layout.js
    日付全部が出る設定に変えた

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
+          © {new Date().toString()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
