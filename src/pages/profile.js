f5665df7cd187d305f14eb79455133d3effdb2e3
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2021-08-07 18:52:38 +0900

    Update profile.js
    SEO追加

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import Seo from '../components/seo';
 
 const ProfilePage = () => (
   <Layout>
