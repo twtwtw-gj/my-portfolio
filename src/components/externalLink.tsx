35309efa605a39113a4339e62adb7b3d621b5d31
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2021-08-07 18:48:57 +0900

    External link別タブ修正

--- src/components/externalLink.tsx
+++ src/components/externalLink.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import urlPropType from 'url-prop-type';
 
-const ExternalLink = ({ url, text }) => <a href={url}>{text}</a>;
+const ExternalLink = ({ url, text }) => (
+  <a href={url} target="_blank" rel="noopener noreferrer">
+    {text}
+  </a>
+);
 
 ExternalLink.propTypes = {
   text: PropTypes.string.isRequired,
