eab3b315d48b1ba46e0b9a29d3ef44f94e595946
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2021-08-09 14:32:33 +0900

    使用してないLinkコンポーネントのインポート削除

--- src/pages/site.tsx
+++ src/pages/site.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PageProps, Link } from 'gatsby';
+import { PageProps } from 'gatsby';
 
 import Layout from '../components/layout';
 import Seo from '../components/seo';
