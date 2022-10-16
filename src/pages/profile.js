60b5999cb1380fd94b56ba6d9eaa44fbd5779039
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2022-10-16 17:49:38 +0900

    東京都コロナサイトについての記述を削除

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -10,14 +10,6 @@ const ProfilePage = ({ location }) => (
     <h1>プロフィール</h1>
     <p>twtwtw-gjです。</p>
     <p>日本・東京・多摩地域に住んでいます</p>
-    <p>
-      その関係で、
-      <ExternalLink
-        text="東京都のCovid-19対策サイトの開発現場"
-        url="https://github.com/tokyo-metropolitan-gov/covid19"
-      />
-      もちょくちょく見学させていただいてます。
-    </p>
     <br />
     <p>大学・大学院では数学を学んでおりました。特に代数学・数学基礎論・集合論あたりを専攻していました。</p>
   </Layout>
