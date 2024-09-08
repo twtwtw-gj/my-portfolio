af475acee389f328f062bf443292ad006d92a664
twtwtw-gj <twtwtw-gj@users.noreply.github.com>
2024-09-08 13:34:58 +0900

    deploy: d6794ee84f391ff7b2e496cd9c2d6885364c9384

---
+++ component---src-pages-404-js-1e17c36c3012992d0cf1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_portfolio=self.webpackChunkmy_portfolio||[]).push([[125],{3331:function(e,t,o){o.r(t);var l=o(6540),n=o(1221),u=o(2269);t.default=e=>{let{location:t}=e;return l.createElement(n.A,{location:t},l.createElement(u.A,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1e17c36c3012992d0cf1.js.map
